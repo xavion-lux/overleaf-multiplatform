41b24930feee7c1cb785c595af7301123a773acc
Xavi <xavion.lux@gmail.com>
2024-10-02 06:24:00 +0000

    Update genScript.js

--- server-ce/genScript.js
+++ server-ce/genScript.js
@@ -12,9 +12,17 @@ switch (process.argv.pop()) {
       console.log('pushd', `services/${service.name}`)
       switch (service.name) {
         case 'web':
+          // Avoid downloading of cypress
+          console.log('export CYPRESS_INSTALL_BINARY=0')
+
+          // install webpack and frontend dependencies
+          console.log('npm install --include=dev')
+          // run webpack
           console.log('npm run webpack:production')
-          // drop webpack/babel cache
-          console.log('rm -rf node_modules/.cache')
+          // uninstall webpack and frontend dependencies
+          console.log('npm install --omit=dev')
+          // precompile pug
+          console.log('npm run precompile-pug')
           break
         default:
           console.log(`echo ${service.name} does not require a compilation`)
